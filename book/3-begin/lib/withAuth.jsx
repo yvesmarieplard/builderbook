48d3835449095e48095d418a1789c95dd556f97e
yvesmarieplard <72050136+yvesmarieplard@users.noreply.github.com>
2022-04-09 15:28:49 -0700

    Chap3

--- book/3-begin/lib/withAuth.jsx
+++ book/3-begin/lib/withAuth.jsx
@@ -5,61 +5,79 @@ import Router from 'next/router';
 let globalUser = null;
 
 export default function withAuth(
-    BaseComplonent, 
-    { loginRequired= true, logoutRequired = false} = {},
+  BaseComponent,
+  { loginRequired = true, logoutRequired = false } = {},
 ) {
-    const propTypes = {
-        user: PropTypes.shape({
-            id: PropTypes.string, 
-            isAdmin; PropTypes.bool,
-        }),
-        isFromServer: PropTypes.bool.isRequired,
-    };
-    
-    const defaultProps= {user: null,
-    };
+  class App extends React.Component {
+    static async getInitialProps(ctx) {
+      const isFromServer = typeof window === 'undefined';
+      const user = ctx.req ? ctx.req.user && ctx.req.user.toObject() : globalUser;
 
-    class App extends React.Component {
-        static async getInitialProps(ctx){
-            // 1. getInitialProps
-            const isFromServer = typeof window === 'undefined';
-            const user = ctx.req ? ctx.req.user && ctx.req.user.toObject(): globalUser;
-            
-            if (isFromServer && user){
-                console.log('before', typeof user._id, user._id);
-                user._id = user._id.toString();
-                console.log('after', typeof user._id, user._id);
-            }
-            
-            const props = {user, isFromServer};
+      if (isFromServer && user) {
+        console.log('before', typeof user._id, user._id);
+        user._id = user._id.toString();
+        console.log('after', typeof user._id, user._id);
+      }
 
-            if (BaseComponent.getInitialProps){
-                Object.assign(props, (await BaseComponent.getInitialProps(ctx)) || {})
-            }
+      const props = { user, isFromServer };
 
-            return props;
+      if (BaseComponent.getInitialProps) {
+        Object.assign(props, (await BaseComponent.getInitialProps(ctx)) || {});
+      }
 
-        }
-
-
-        componentDidMount(){
-            // 2. componentDidMount
-        }
-        
-        render(){
-            // 3. render
-
-            return (
-                <>
-                    <BaseComponent {...this.props} />
-                </>
-            );
-        }
-
+      return props;
     }
 
-    App.propTypes = propTypes;
-    App.defaultProps = defaultProps;
+    componentDidMount() {
+      const { user, isFromServer } = this.props;
 
-    return App;
+      if (isFromServer) {
+        globalUser = user;
+      }
+
+      /*if (loginRequired && !logoutRequired && !user) {
+        Router.push('/public/login', '/login');
+        return;
+      }*/
+
+      if (logoutRequired && user) {
+        Router.push('/');
+      }
+    }
+
+    render() {
+      const { user } = this.props;
+
+      if (loginRequired && !logoutRequired && !user) {
+        return null;
+      }
+
+      if (logoutRequired && user) {
+        return null;
+      }
+
+      return (
+        <>
+          <BaseComponent {...this.props} />
+        </>
+      );
+    }
+  }
+
+  const propTypes = {
+    user: PropTypes.shape({
+      id: PropTypes.string,
+      isAdmin: PropTypes.bool,
+    }),
+    isFromServer: PropTypes.bool.isRequired,
+  };
+
+  const defaultProps = {
+    user: null,
+  };
+
+  App.propTypes = propTypes;
+  App.defaultProps = defaultProps;
+
+  return App;
 }
--- book/3-begin/server/server.js
+++ book/3-begin/server/server.js
@@ -55,9 +55,9 @@ app.prepare().then(() => {
 
   // this is test code, it will be removed by the end of Chapter 3
   server.get('/', async (req, res) => {
-    req.session.foo = 'bar';
     const user = await User.findOne({ slug: 'team-builder-book' });
-    app.render(req, res, '/', { user });
+    req.user = user;
+    app.render(req, res, '/');
   });
 
   server.get('*', (req, res) => handle(req, res));
